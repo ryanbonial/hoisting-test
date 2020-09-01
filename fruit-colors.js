95ba5924560c1278fbb76ca5644359ee945ed9ae
Ryan Bonial <ryanbonial@gmail.com>
2020-09-01 15:53:20 -0600

    even deeper

--- fruit-colors.js
+++ fruit-colors.js
@@ -1,8 +1,5 @@
 import {banana, grape, apple } from './fruit';
 
-import * as idx from './index';
-
-console.log(idx.greeting)
 const COLORS = [banana, grape, apple];
 export function doThings() {
   return `${COLORS[0]} ${COLORS[1]} ${COLORS[2]}`
--- fruit.js
+++ fruit.js
@@ -1,10 +1,12 @@
 export { banana, grape, apple };
 
+import * as idx from './index';
+
+console.log(idx.greeting)
+
 const banana = 'yellow';
 const grape = 'purple'
-
-
-  const apple = 'steve'
+const apple = 'steve'
 
 
 
